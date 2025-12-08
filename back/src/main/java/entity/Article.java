ea3592e5eb7c5c0ac1da4cd52eed5c34881b140c
jihoon <dlakstddl@gmail.com>
2025-12-08 16:06:00 +0900

    Add root

---
+++ back/src/main/java/entity/Article.java
@@ -0,0 +1,47 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long articleId;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime publishedAt;
+
+    private Integer viewCount;
+
+    private Float trustWeight;
+
+    @ManyToOne
+    @JoinColumn(name = "theme_id")
+    private Theme theme;
+
+    @ManyToOne
+    @JoinColumn(name = "reporter_id")
+    private Reporter reporter;
+}
