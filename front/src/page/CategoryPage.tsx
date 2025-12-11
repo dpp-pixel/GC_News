b69ad4e76edd1edd1bfdbec059cdb9b3f013b9dc
yuseong <dltbtjd7@gmail.com>
2025-12-11 12:44:59 +0900

    점심 전

--- front/src/page/CategoryPage.tsx
+++ front/src/page/CategoryPage.tsx
@@ -1,16 +1,45 @@
-import NaverNews from "../components/mainpage/NaverNews";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
-interface Props {
-  category: string;
+interface NewsItem {
+  title: string;
+  link: string;
+  description: string;
+  pubDate: string;
 }
 
-export default function CategoryPage({ category }: Props) {
+export default function CategoryPage() {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const category = params.get("type") || "정치";
+
+  const [news, setNews] = useState<NewsItem[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/api/news", {
+        params: { query: category }
+      })
+      .then((res) => {
+        setNews(res.data.items); // ⭐ NaverNews와 동일한 구조
+      })
+      .catch((err) => console.error("API 오류:", err));
+  }, [category]);
+
   return (
-    <div style={{ padding: "20px" }}>
+    <div>
       <h2>{category} 뉴스</h2>
 
-      {/* 해당 카테고리 뉴스만 가져오기 */}
-      <NaverNews query={category} />
+      {news.length === 0 && <p>뉴스를 불러오는 중...</p>}
+
+      {news.map((item, idx) => (
+        <div key={idx}>
+          <a href={item.link} target="_blank" rel="noreferrer">
+            {item.title}
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
